2013be6cf5d35dfa68ab90caf477f5b77ab6491d
animatedb <animatedb@users.noreply.github.com>
2019-04-07 08:25:35 -0700

    Create PlotSolar.py

---
+++ code/PlotSolar.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import csv
+
+# Column A is date, O is generation/day, P is Use/day
+def plotDailyGenUse(fn):
+    with open(fn, 'r') as f:
+        csvData = csv.reader(f)
+        dateData = []
+        genData = []
+        useData = []
+        for row in csvData:
+            genIndex = ord('O')-ord('A')
+            if row[genIndex] and row[0] != 'Date':
+                dateData.append(row[0])
+                genData.append(float(row[genIndex]))
+                useData.append(float(row[ord('P')-ord('A')]))
+#	print(dateData)
+#	print(genData)
+#        plt.locator_params(axis='x', nbins=12)	# doesn't work for dates
+
+	fig = plt.figure()
+	fig.set_size_inches(10, 7)
+	ax = fig.gca()
+        fig.autofmt_xdate()
+        ax.grid(color='#eeeeee')
+	ax.set_axisbelow(True)
+	ax.tick_params(axis='x', rotation=65)
+
+
+        plt.scatter(dateData, genData, label='Generation/Day')
+	plt.plot(dateData, genData)
+        plt.scatter(dateData, useData, label='Use/Day')
+	plt.plot(dateData, useData)
+
+        ax.legend()
+
+#        plt.xlabel('Date')
+        plt.ylabel('kWh')
+#        plt.savefig('SolGenUse.png')
+        plt.savefig('SolGenUse.svg')
+
+def plotTotalSurplus(fn):
+    with open(fn, 'r') as f:
+        csvData = csv.reader(f)
+        dateData = []
+        surpData = []
+        for row in csvData:
+            surpIndex = ord('R')-ord('A')
+            if row[surpIndex] and row[0] != 'Date':
+                dateData.append(row[0])
+                surpData.append(float(row[surpIndex]))
+	fig = plt.figure()
+	fig.set_size_inches(10, 7)
+	ax = fig.gca()
+        fig.autofmt_xdate()
+        ax.grid(color='#eeeeee')
+	ax.set_axisbelow(True)
+	ax.tick_params(axis='x', rotation=65)
+        plt.scatter(dateData, surpData, label='Balance')
+	plt.plot(dateData, surpData)
+        ax.legend()
+        plt.ylabel('kWh')
+        plt.savefig('SolBalance.svg')
+
+
+plotDailyGenUse('SolarGeneration.csv')
+plotTotalSurplus('SolarGeneration.csv')
+
